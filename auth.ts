157e62961c8b91bd382d870c8c3a5ef9c2ad920a
thealindsjo <thea.lindsjo@gmail.com>
2025-10-28 21:45:26 +0100

    Update Google authentication provider environment variables: Change clientId and clientSecret to use AUTH_GOOGLE_ID and AUTH_GOOGLE_SECRET for correct configuration

--- auth.ts
+++ auth.ts
@@ -4,8 +4,8 @@ import Google from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
   pages: {
