5fc843e8f90cd756eea7926e354e936e0d7905d1
thealindsjo <thea.lindsjo@gmail.com>
2025-10-29 11:01:22 +0100

    idk

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
@@ -18,10 +19,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
       if (isOnCountryPage) {
         if (isLoggedIn) return true;
-        return false; 
+        return false;
       }
 
       return true;
     },
   },
+  trustHost: true, // Important for Vercel deployments
 });
