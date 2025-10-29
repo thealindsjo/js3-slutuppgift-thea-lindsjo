b72ef868c14fb007531d4b2fc4a5103af8dde16f
thealindsjo <thea.lindsjo@gmail.com>
2025-10-29 11:16:24 +0100

    adrian auth

--- auth.ts
+++ auth.ts
@@ -2,25 +2,5 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  secret: process.env.AUTH_SECRET,
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnCountryPage = nextUrl.pathname.startsWith("/country/");
-
-      if (isOnCountryPage) {
-        if (isLoggedIn) return true;
-        return false;
-      }
-
-      return true;
-    },
-  },
-  trustHost: true, // Important for Vercel deployments
+  providers: [Google],
 });
