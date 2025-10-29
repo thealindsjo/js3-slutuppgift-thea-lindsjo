c06ad6c03c69b4c8991325804674d082415341c8
thealindsjo <thea.lindsjo@gmail.com>
2025-10-29 13:10:51 +0100

    Refactor NextAuth configuration: Simplify Google provider setup and remove custom callbacks and pages

--- auth.ts
+++ auth.ts
@@ -2,26 +2,6 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
-  pages: {
-    signIn: "/auth/signin",
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnCountryPage = nextUrl.pathname.startsWith("/country/");
-
-      if (isOnCountryPage) {
-        if (isLoggedIn) return true;
-        return false; 
-      }
-
-      return true;
-    },
-  },
+  providers: [Google],
 });
+
